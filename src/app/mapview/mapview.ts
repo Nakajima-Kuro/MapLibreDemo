02f9c4732ed317cb5d81de323f7ec4f49a08e525
Nakajima Kuro <tnnamanh@gmail.com>
2025-09-16 09:32:21 +0700

    Implement Maptiler

--- src/app/mapview/mapview.ts
+++ src/app/mapview/mapview.ts
@@ -1,4 +1,9 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, OnDestroy } from '@angular/core';
+// import * as maplibregl from '@maptiler/sdk';
+// import { Map } from '@maptiler/sdk';
+// import '@maptiler/sdk/dist/maptiler-sdk.css';
+
+import * as maplibregl from 'maplibre-gl';
 import { Map } from 'maplibre-gl';
 import 'maplibre-gl/dist/maplibre-gl.css';
 
@@ -8,23 +13,58 @@ import 'maplibre-gl/dist/maplibre-gl.css';
   templateUrl: './mapview.html',
   styleUrl: './mapview.css',
 })
-export class Mapview implements AfterViewInit {
+export class Mapview implements OnInit, AfterViewInit, OnDestroy {
+  map: Map | undefined;
+
+  @ViewChild('map')
+  private mapContainer!: ElementRef<HTMLElement>;
+
   constructor() {}
+
+  ngOnInit(): void {
+    // config.apiKey = 'TklZTc88Zw2IBk5gacCp';
+  }
 
   ngAfterViewInit(): void {
     setTimeout(() => {
       this.initMap();
-    }, 0);
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.map?.remove();
   }
 
   initMap(): void {
     // Initialize the map here using MapLibre GL JS
-    const initialState = { lng: 105, lat: 16, zoom: 5 };
-    const map = new Map({
-      container: 'map', // container ID
-      style: `https://api.maptiler.com/maps/basic-v2/style.json?key=I9wWU1dnlxq4iLDF2WfM&mtsid=ab4bc823-7da3-4a25-95ad-57c780bed5da`,
-      center: [70, 0], // starting position [lng, lat]
-      zoom: 2, // starting zoom
+    const initialState = { lng: 106, lat: 16, zoom: 5 };
+    this.map = new Map({
+      container: this.mapContainer.nativeElement, // container ID
+      style:
+        'https://api.maptiler.com/maps/satellite/style.json?key=I9wWU1dnlxq4iLDF2WfM&mtsid=ab4bc823-7da3-4a25-95ad-57c780bed5da',
+      center: [initialState.lng, initialState.lat], // starting position [lng, lat]
+      zoom: initialState.zoom, // starting zoom,
+    });
+
+    // Add scale control to the map.
+    let scale = new maplibregl.ScaleControl({
+      maxWidth: 80,
+      unit: 'metric',
+    });
+    this.map.addControl(scale, 'bottom-right');
+
+    // Add zoom and rotation controls to the map.
+    this.map.addControl(
+      new maplibregl.NavigationControl({
+        visualizePitch: true,
+        visualizeRoll: true,
+        showZoom: true,
+        showCompass: true,
+      })
+    );
+
+    this.map.on('load', () => {
+      console.log('Map has been loaded');
     });
   }
 }
